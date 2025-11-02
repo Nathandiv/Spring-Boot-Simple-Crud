da4aacb92fa8f0214d213bf43b05a2f883d3ca74
Nathandiv <nathan.hlungwani123@gmail.com>
2025-11-02 17:49:13 +0200

    added logic on the controller and goballExeception

--- src/main/java/com/example/crud_app/controller/UserController.java
+++ src/main/java/com/example/crud_app/controller/UserController.java
@@ -1,4 +1,57 @@
 package com.example.crud_app.controller;
 
+import com.example.crud_app.entity.User;
+import com.example.crud_app.exception.DuplicateEmailException;
+import com.example.crud_app.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService service;
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<User> create(@Valid @RequestBody User user) {
+        User created = service.create(user);
+        return ResponseEntity.status(201).body(created); // 201 Created
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<User>> searchByName(@RequestParam(required = false) String name) {
+        List<User> users = service.findByName(name);
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @Valid @RequestBody User user) {
+        User updated = service.update(id, user);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build(); // 204 No Content
+    }
+
+
 }
