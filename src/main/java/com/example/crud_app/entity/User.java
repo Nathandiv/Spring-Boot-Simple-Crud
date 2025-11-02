c4d9064e90399a6b38b2c4215a40c938b7df9601
Nathandiv <nathan.hlungwani123@gmail.com>
2025-11-02 12:12:15 +0200

    added entity logic

--- src/main/java/com/example/crud_app/entity/User.java
+++ src/main/java/com/example/crud_app/entity/User.java
@@ -1,4 +1,15 @@
 package com.example.crud_app.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
 }
